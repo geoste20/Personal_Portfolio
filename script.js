e12705ec57deccac424954e3e32c8ceb907c88af
gstefanis20 <gstefanis@horizon.csueastbay.edu>
2026-06-10 11:48:13 -0400

    Add comments and mobile navigation

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 if (window.location.search.includes("success=true")) {
     document.getElementById("success-message").style.display = "block";
 }
+
+//Dropdown Menu for mobile device(bootstrap) 
+const mobileMenu = document.querySelector(".mobile-menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+if (mobileMenu && navLinks) {
+    mobileMenu.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+}
